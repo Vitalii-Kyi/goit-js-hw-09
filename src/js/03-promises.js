9acb0865938640027f67f8dde8a32dd92f71cede
Vitalii Kyi <kyi.profy@gmail.com>
2023-06-28 16:01:16 +0300

    hw-09: fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -37,10 +37,10 @@ inputForm.addEventListener('submit', event => {
     for (let i = 0; i < amount; i++) {
       createPromise(i, delay + step * i)
         .then(({ position, delay }) => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notify.success(`✅ Fulfilled promise ${position +1} in ${delay}ms`);
         })
         .catch(({ position, delay }) => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          Notify.failure(`❌ Rejected promise ${position +1} in ${delay}ms`);
         });
     }
   }
